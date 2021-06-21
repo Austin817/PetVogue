ef45a8e43bc2fec41a34d0de04b9138851869fb4
Linzhe470 <linzhe470@gmail.com>
2021-06-21 17:24:14 +0800

    checkcss

--- resources/views/layouts/limo.blade.php
+++ resources/views/layouts/limo.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 
     <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
