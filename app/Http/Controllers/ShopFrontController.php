ce62fe62a90eb390fa1b92a5d7ff43e304aeee40
Austin817 <81277678+Austin817@users.noreply.github.com>
2021-06-25 15:13:17 +0800

    Add files via upload

---
+++ app/Http/Controllers/ShopFrontController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use App\ProductImg;
+use Illuminate\Http\Request;
+
+class ShopFrontController extends Controller
+{
+    //
+    public function index(Request $request)
+    {
+        // if($request->typeId){
+
+        //     $productsData = Product::with('linkProductType')->where('type_id',$request->typeId)->get();
+
+        // }else{
+
+        //     $productsData = Product::with('linkProductType')->get();
+        // }
+        $productsData = Product::with('linkProductType')->get();
+        $id = $request->typeId;
+        return view('front.shop.shop_index',compact('productsData','id'));
+    }
+
+
+
+    public function detail($id)
+    {
+        $productData = Product::with('linkProductType','linkProductImg')->find($id);
+        return view('front.shop.shop_detail',compact('productData'));
+    }
+
+}
+
